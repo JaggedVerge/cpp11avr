cc60e8f1b48bd6f6595c11ab9bd5466e3161b347
Janis Lesinskis <janis@lesinskis.com>
2014-07-01 22:43:20 -0400

    Added more explanation in comments about __cxa_pure_virtual

--- src/libavrcpp_impl.cpp
+++ src/libavrcpp_impl.cpp
@@ -2,11 +2,14 @@
 
 #include "libcpp_avr_extension.h"
 
-/** This is only ever invoked in an error condition.
-Such an error can be caused by if a call to a virtual function
+/** __cxa_pure_virtual is called when a pure-virtual function
+is called. Note that this is only ever invoked in an error condition.
+Such an error can be caused if a call to a virtual function
 is made before the object is fully constructed.
 Because we cannot handle this case gracefully we have to abort the
-program.
+program. Note that the call to abort() here could be substituted
+for some other error reporting mechanism when used in your project,
+abort() is just a default.
 */
 void __cxa_pure_virtual(void){
 	abort();
